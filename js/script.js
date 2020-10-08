0b79cc299afb4915ca284a9d76340a6a364ae980
bell93033 <72568851+bell93033@users.noreply.github.com>
2020-10-08 13:59:39 -0700

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-nice-p-r-i-c-e-12oo","displayName":"nice p r i c e - $ 12OO","link":{"linkType":"LinkTypePage","href":"#!page-nice-p-r-i-c-e-12oo"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
